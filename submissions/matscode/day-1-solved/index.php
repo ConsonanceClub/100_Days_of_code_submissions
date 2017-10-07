359bc7270bedb8714b9a429f9dad7e9eee17b7f7
Michael Akanji <matscode@gmail.com>
2017-10-07 14:26:09 +0100

    Day 1 Challenge Solved :-D

---
+++ submissions/matscode/day-1-solved/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * Description
+ *
+ * @package        100 Days of Code
+ * @category       Source
+ * @author         Michael Akanji <matscode@gmail.com> {@link michaelakanji.com}
+ * @date           2017-10-07
+ * @copyright (c)  2016 - 2017, TECRUM (http://www.tecrum.com)
+ *
+ */
+
+require 'number-guess-game.php';
