094ac411ae5e158ed157dd35e253c0749a6c9788
Dah_Kul_Ehmbeey <bellomuba.rak0@gmail.com>
2017-10-09 00:12:27 +0100

    Completed all tasks up to date

--- submissions/DahKulEhmbeey/Day1_task.py
+++ submissions/DahKulEhmbeey/Day1_task.py
@@ -1,11 +1,42 @@
-class task1(object):
-	"""docstring for task1"""
-	def __init__(self, arg):
-		super(task1, self).__init__()
-		self.arg = arg
-	def get_input():
-		user_input = input("Please enter a number")
-		return user_input
-	def check_input(get_input()):
+import random
+num_generated = random.randint(1, 10)
+counter = 0
 
-task1 = task1()
+
+def get_input():
+    global counter
+    if counter == 0:
+        counter += 1
+        try:
+            user_input = int(input("Please enter your guess: "))
+        except ValueError:
+            print("That's an invalid input.\n")
+            get_input()
+        else:
+            check_guess(user_input)
+    else:
+        counter += 1
+        try:
+            user_input = int(input("Enter another guess: "))
+        except ValueError:
+            print("That's an invalid input.\n")
+            get_input()
+        else:
+            check_guess(user_input)
+
+
+def check_guess(user_input):
+    global num_generated
+    if user_input == num_generated:
+        print("Bravo!!! Your guess is correct.")
+        print("Got the answer after {} trials.".format(counter))
+    elif user_input > num_generated:
+        print("That's high man!!! I'll give you another chance.\n")
+        get_input()
+    else:
+        print("Too low! Something else?\n")
+        get_input()
+
+
+print("Guess My Number game.\n")
+get_input()
---
+++ submissions/DahKulEhmbeey/Day4_task.py
@@ -0,0 +1,20 @@
+def get_input():
+    user_input = input("Please enter a value: ")
+    check_palindrome(user_input)
+
+
+def check_palindrome(user_input):
+    end_point = len(user_input)-1
+    mid_point = int(len(user_input)/2)
+    first_half = user_input[:mid_point]
+    if len(user_input) % 2 == 1: # if it it odd
+        other_half = user_input[end_point:mid_point:-1]
+    else: # if it is even
+        other_half = user_input[end_point:mid_point-1:-1]
+    if first_half == other_half:
+        print("{} is a palindrome".format(user_input))
+    else:
+        print("Oops!!! {} is not a palindrome".format(user_input))
+
+print("Palindrome checker!!!\n")
+get_input()
