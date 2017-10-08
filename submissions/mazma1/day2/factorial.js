36fe9c34151186004d158ce3e0e4e2d3a733233d
Mary Mazi <mazi.mary.o@gmail.com>
2017-10-08 19:04:08 +0100

    Implement finding factorials using recursion and loop methods

---
+++ submissions/mazma1/day2/factorial.js
@@ -0,0 +1,31 @@
+/**
+ * Finds the factorial of a given number using the recursive method
+ * 
+ * @param {any} number 
+ * @returns factorial of given number
+ */
+function factorial(number) {
+  if (number == 1) {
+    return (1);
+  }
+  return (number * factorial(number - 1));
+}
+
+
+/**
+ * Finds the factorial of a given number using a forLoop
+ * 
+ * @param {any} number 
+ * @returns factorial of given number
+ */
+function loopFactorial(number) {
+  let factorial = 1;
+  for (i = 1; i <= number; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+factorial(4);
+loopFactorial(3);
+
