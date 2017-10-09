c36810ff298de387c4836511c7c0e91f5e81b48b
Jennifer Nordell <jennifer.nordell@outlook.com>
2017-10-09 16:54:46 +0200

    accounting for uppercase

--- submissions/j-nordell/Day5/vowelcount.py
+++ submissions/j-nordell/Day5/vowelcount.py
@@ -1,13 +1,12 @@
 vowel_dict = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
 vowels = ['a', 'e', 'i', 'o', 'u']
 user_text = input("Please enter some text:  ")
-user_text = list(user_text)
+user_text = list(user_text.lower())
 
 for letter in user_text:
   if letter in vowels:
     vowel_dict[letter] += 1
   
-
 print("Here are the results: ")
 
 for key, value in vowel_dict.items():
