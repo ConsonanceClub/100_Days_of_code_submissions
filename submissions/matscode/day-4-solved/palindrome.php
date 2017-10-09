b4afc7b69c910b3f9bb0f4661c59b02df9ca3830
Michael Akanji <matscode@gmail.com>
2017-10-09 15:28:47 +0100

    Corrected author link

--- submissions/matscode/day-4-solved/palindrome.php
+++ submissions/matscode/day-4-solved/palindrome.php
@@ -3,7 +3,7 @@
  *
  * @package        100 Days of Code
  * @category       Source
- * @author         Michael Akanji <matscode@gmail.com> {@link michaelakanji.com}
+ * @author         Michael Akanji <matscode@gmail.com> {@link http://michaelakanji.com}
  * @date           2017-10-08
  *
  */
