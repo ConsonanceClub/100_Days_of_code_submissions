09ee245b6be9f4f4137647812bde715accfd8179
belle_meee <adeniranyusufabdullateef@gmail.com>
2017-10-11 12:04:24 +0100

    Day 7

--- submissions/abdullateef97/Day6.py
+++ submissions/abdullateef97/Day6.py
@@ -5,8 +5,8 @@ word = Path('obama.txt').read_text()
 alphabet_list = list(string.ascii_lowercase)
 alphabet_count = [0]*26
 
-# word = input('Enter your word : ')
-i = 0
+
+# i = 0
 for alphabet in alphabet_list:
     count = word.count(alphabet)
     alphabet_count[i] = count
