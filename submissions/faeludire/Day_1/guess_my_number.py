02b7a3d224684e01f43c19a2085f22f1925f5d57
DejEludire <faeludire@gmail.com>
2017-10-18 14:27:02 +0100

    Update submissions

--- submissions/faeludire/Day_1/guess_my_number.py
+++ submissions/faeludire/Day_1/guess_my_number.py
@@ -9,32 +9,30 @@ import random
 
 guessesTaken = 0 #variable keeps track of the number of guesses that have been made
 
-print "Hello, What's your name?"
+print ("Hello, What's your name?")
 myName = raw_input()
 
 number = random.randint(1,20)
 print("Well, "+ myName + ", I am thinking of a number between 1 and 20.")
 
 while guessesTaken < 6:
-    print "Take a guess."
+    print ("Take a guess.")
     guess = input()
     guess = int(guess)
 
     guessesTaken = guessesTaken + 1
 
     if guess < number:
-        print "Your guess is too low"
+        print ("Your guess is too low")
     if guess > number:
-        print "Your guess is too high"
+        print ("Your guess is too high")
     if guess == number:
         break
 
 if guess == number:
     guessesTaken = str(guessesTaken)
-    print "Good Job, " + myName + "! You guessed the number in " + guessesTaken + " guesses!"
+    print ("Good Job, " + myName + "! You guessed the number in " + guessesTaken + " guesses!")
 
 if guess != number:
     number = str(number)
-    print"Nope. The number I was thinking of was" +number
-
-
+    print ("Nope. The number I was thinking of was" + number)
---
+++ submissions/faeludire/Day_2/factorial_finder.py
@@ -0,0 +1,5 @@
+#Day 2: Factorial Finder
+#
+#The Factorial of a positive integer, n, is defined as the product of the sequence
+#n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1.
+#This program finds the factorial of a number using both loops and recursion
