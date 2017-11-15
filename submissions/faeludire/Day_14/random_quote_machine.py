22bf5a8795ae268cd29ce389b6cebb03bc1615d9
Fiyinfolu <faeludire@gmail.com>
2017-11-15 16:49:35 +0100

    Solution for Day 13 & Day 14

---
+++ submissions/faeludire/Day_14/random_quote_machine.py
@@ -0,0 +1,35 @@
+# Day 14: Random Quote Machine
+# Random Quote Machine - Program should print out quotes that have been stored randomly. 
+# You don't need to use any API. Store some quotes in your program,
+# and print one at random each time the program executes.
+
+import random
+
+quotes = {
+    {  
+        'quote': "Strive not to be a success, but rather to be of value.",
+        'author': "Albert Einstein",
+    },
+    {
+        'quote': "Two roads diverged in a wood, and I—I took the one less traveled by, \
+        And that has made all the difference.",
+        'author': "Robert Frost",
+    },
+    {
+        'quote': "I attribute my success to this: I never gave or took any excuse.",
+        'author': "Florence Nightingale",
+    },
+    {
+        'quote': "You miss 100% of the shots you don’t take.",
+        'author': "Wayne Gretzky",
+    },
+    {
+        'quote': "The most difficult thing is the decision to act, the rest is merely tenacity.",
+        'author': "Amelia Earhart",
+    },
+    {
+        'quote': "Every strike brings me closer to the next home run.",
+        'author': "Babe Ruth",
+    },
+}
+
